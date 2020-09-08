2641eba63fa8ee3951a65277f8ef113f62bb86d1
nichro02 <nichro02@gmail.com>
2020-09-08 16:06:15 -0700

    added h1 using js

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 */
 
+
+//create variable for container div
+const container = document.querySelector("#container")
+
+//create h1 element and set to variable, append to container
+
+const h1 = document.createElement("h1")
+h1.innerText = "Hogwarts"
+
+container.appendChild(h1)
